fa2f762b95ed9717e18c4a362b5456661280e610
TUQ <tuq@elca.vn>
2020-09-10 17:51:18 +0700

    Working on Main Window

---
+++ ViewModel/ControlBarViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WarehouseManagement.ViewModel
+{
+    public class ControlBarViewModel : BaseViewModel
+    {
+        public ICommand CloseWindowCommand { get; set; }
+        public ControlBarViewModel()
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>((p) => { return p == null; } , (p)=>
+            { 
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    w.Close();
+                }
+            });
+        }
+
+        FrameworkElement GetWindowParent(UserControl p)
+        {
+            FrameworkElement parent = p;
+
+            while (parent.Parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+
+            return parent;
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WarehouseManagement.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool IsLoaded { get; set; }
+        public MainViewModel()
+        {
+            if (!IsLoaded)
+            {
+                LoginWindow loginWindows = new LoginWindow();
+                loginWindows.ShowDialog();
+                IsLoaded = true;
+            }
+        }
+    }
+}
